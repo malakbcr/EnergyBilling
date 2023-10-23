0fb30c3a672f7a3ddb18d10c54747137f7f3461d
malakbcr <baccarmalak@gmail.com>
2023-10-23 13:02:37 +0200

    Edit return message

--- EnergyBilling/src/main/java/com/example/EnergyBilling/Exceptions/InvalidReferenceException.java
+++
@@ -1,7 +0,0 @@
-package com.example.EnergyBilling.Exceptions;
-
-public class InvalidReferenceException extends RuntimeException {
-    public InvalidReferenceException (String message) {
-        super(message) ;
-    }
-}
